e0091c80e923dd027cad4738113ee5d69bdeb8a6
kevinxiao712 <66409722+kevinxiao712@users.noreply.github.com>
2025-02-20 15:15:01 -0500

    setup

--- Supermarketshooter/Assets/Scripts/Playermovement.cs
+++ Supermarketshooter/Assets/Scripts/Playermovement.cs
@@ -1,16 +1,177 @@
 using UnityEngine;
+using System.Collections;
 
 public class Playermovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    
+    [Header("Movement")]
+    private float MoveSpeed;
+    public float walkSpeed;
+    public float sprintSpeed;
+    public float groundDrag;
+    public float jumpforce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump;
+
+
+    [Header("Keys")]
+    public KeyCode jumpKey = KeyCode.Space;
+    public KeyCode sprintKey = KeyCode.LeftShift;
+
+    [Header("GroundCheck")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    public Transform orientation;
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+
+
+    private float maxFallSpeed = 0f;
+    private bool wasGrounded = true;
+
+
+
+    [Header("Slope Hnadling")]
+    public float maxSlopeAngle;
+    private RaycastHit slopHit;
+    private bool exitingSlope;
+    Rigidbody rb;
+    public MovementState state;
+
+
+
+    public enum MovementState
     {
-        
+        walking,
+        sprinting,
+        air
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        readyToJump = true;
     }
+
+        public void Update()
+    {
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+        wasGrounded = grounded;
+        MyInput();
+        SpeedControl();
+        StateHandler();
+        if (grounded)
+        {
+            rb.linearDamping = groundDrag;
+        }
+        else
+            rb.linearDamping = 0;
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+        if (Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+            Jump();
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+    
+    public void MovePlayer()
+    {
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (OnSlope() && !exitingSlope)
+        {
+            rb.AddForce(GetSlopeMoveDirection() * MoveSpeed * 20f, ForceMode.Force);
+            if(rb.linearVelocity.y > 0)
+                rb.AddForce(Vector3.down * 80f , ForceMode.Force);
+        }
+        if (grounded)
+            rb.AddForce(moveDirection.normalized * MoveSpeed * 10f, ForceMode.Force);
+        else if (!grounded)
+            rb.AddForce(moveDirection.normalized * MoveSpeed * 10f * airMultiplier, ForceMode.Force);
+
+        rb.useGravity = !OnSlope();
+
+    }
+
+    private void SpeedControl()
+    {
+        if (OnSlope() && !exitingSlope)
+        {
+            if (rb.linearVelocity.magnitude > MoveSpeed)
+                rb.linearVelocity = rb.linearVelocity.normalized * MoveSpeed;
+        }
+        else
+        {
+            Vector3 flatVel = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+            if (flatVel.magnitude > MoveSpeed)
+            {
+                Vector3 limitedVel = flatVel.normalized * MoveSpeed;
+                rb.linearVelocity = new Vector3(limitedVel.x, rb.linearVelocity.y, limitedVel.z);
+            }
+        }
+    }
+
+
+    private void Jump()
+    {
+        exitingSlope = true;
+        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
+        rb.AddForce(transform.up * jumpforce, ForceMode.Impulse);
+    }
+
+
+    private void ResetJump()
+    {
+        readyToJump = true;
+        exitingSlope = false;
+    }
+
+
+    private bool OnSlope()
+    {
+        if (Physics.Raycast(transform.position, Vector3.down, out slopHit, playerHeight * 0.5f + 0.3f))
+        {
+            float angle = Vector3.Angle(Vector3.up, slopHit.normal);
+            return angle < maxSlopeAngle && angle != 0;
+        }
+        return false;
+    }
+
+
+    private Vector3 GetSlopeMoveDirection()
+    {
+        return Vector3.ProjectOnPlane(moveDirection, slopHit.normal).normalized;
+    }
+
+
+    private void StateHandler()
+    {
+        if(grounded && Input.GetKey(sprintKey))
+        {
+            state = MovementState.sprinting;
+            MoveSpeed = sprintSpeed;
+        }
+        else if(grounded)
+        {
+            state = MovementState.walking;
+            MoveSpeed= walkSpeed;
+        }
+        else
+        {
+            state = MovementState.air;
+        }
+    }
+
 }
