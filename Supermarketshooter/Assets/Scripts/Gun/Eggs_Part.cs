092005567a39bec395c0180cbb078acd1421d1ec
elad flaishon <46261914+elad0211@users.noreply.github.com>
2025-02-28 00:55:20 -0500

    added part bases and added fork to gun

---
+++ Supermarketshooter/Assets/Scripts/Gun/Eggs_Part.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Eggs_Part : Gun_Piece_Base
+{
+    public override void ApplyStateEffects()
+    {
+
+        // Apply visual or gameplay changes based on state
+        switch (currentState)
+        {
+            case GunPieceState.Forward:
+                gun.allowButtonHold = allowButtonHold;
+                gun.shootForce = shootForce;
+                gun.timeBetweenShooting = timeBetweenShooting;
+
+                // Example: Modify gun stats
+                break;
+            case GunPieceState.Mid:
+                Debug.Log("add damage");
+                // Example: Neutral state
+                break;
+            case GunPieceState.Back:
+                gun.timeBetweenShots = timeBetweenShots;
+                gun.bulletsPerTap = bulletsPerTap;
+                gun.spread = spread;
+                gun.magazineSize = magazineSize;
+                // Example: Debuff or different behavior
+                break;
+        }
+    }
+}
